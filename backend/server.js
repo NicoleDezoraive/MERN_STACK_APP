8bba883c1d6b1eb6e7d4e8513a69bdf7a0f581d2
NicoleDezoraive <49281896+NicoleDezoraive@users.noreply.github.com>
2023-06-25 16:31:16 +0300

    add cors

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+var cors = require('cors')
 const express = require('express');
 const mongoose = require('mongoose');
 const workoutRoutes = require('./routes/workouts');
